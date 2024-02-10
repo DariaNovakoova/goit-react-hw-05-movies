2dc6e6629d0f26087e07e89cf88412233bb45504
DariaNovakoova <133808096+DariaNovakoova@users.noreply.github.com>
2024-02-10 17:35:14 +0000

    Deploying to gh-pages from @ DariaNovakoova/goit-react-hw-05-movies@ad2cd3807d508068879168825bbf049017a7f7c6 🚀

---
+++ static/js/554.ec8418c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[554],{554:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r="not-found-page_title__S4jvl",a=n(87),o=n(184),c=function(){return(0,o.jsxs)("div",{className:r,children:[(0,o.jsx)("h1",{children:"Cannot found this page"}),(0,o.jsx)(a.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=554.ec8418c9.chunk.js.map
